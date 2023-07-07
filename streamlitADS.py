e343f3e0c4027f8e1a5af360f1f216ef9b304840
irohitsaha <103872396+irohitsaha@users.noreply.github.com>
2023-07-08 01:46:31 +0530

    Update streamlitADS.py

--- streamlitADS.py
+++ streamlitADS.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 st.title("Automatic Data Analyser")
 
@@ -15,6 +18,12 @@ if upload_file is not None:
 
         st.subheader("Description of Your DataSet : ")
         st.write(df.describe())
+        st.write("Handling Null Values:")
+        null_counts = df.isnull().sum()
+        st.write("Null Value Counts:")
+        st.write(null_counts)
+
+        df.dropna(inplace=True)
 
         st.write("**Data Visualization**")
 
@@ -39,6 +48,38 @@ if upload_file is not None:
         fig, ax = plt.subplots()
         ax.scatter(df[x_axis], df[y_axis])
         st.pyplot(fig)
+
+        st.subheader("**Logistic Regression**")
+
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        model = LogisticRegression()
+        model.fit(X_train, y_train)
+
+        st.write("Dataset is: ")
+        st.dataframe(df)
+
+        y_pred = model.predict(X_test)
+        accuracy_score = model.score(X_test, y_test)
+        st.write("Model Accuracy Score:", accuracy_score)
+
+        report = classification_report(y_test, y_pred)
+        st.write("Classification Report:")
+        st.text(report)
+
+        st.write("Please enter the values for prediction:")
+        prediction_inputs = []
+        for column in X.columns:
+            value = st.number_input(column)
+            prediction_inputs.append(value)
+        
+        prediction = model.predict([prediction_inputs])
+
+        st.write("Prediction:")
+        st.write(prediction)
 
 #Done by Rohit
 
